d8ef01aebf24e2783fbfee72e0d4d65263e11b8f
Olivia Pasion <103903730+Olivia-Pasion@users.noreply.github.com>
2022-08-23 12:49:07 -0700

    Initial commit

---
+++ app.js
@@ -0,0 +1,96 @@
+import { getUser, signOut } from './services/auth-service.js';
+import { getAllTodos, createTodo, updateTodo, deleteTodo } from './services/todo-service.js';
+import { protectPage } from './utils.js';
+import createUser from './components/User.js';
+import createTodos from './components/Todos.js';
+import createAddTodo from './components/AddTodo.js';
+
+// State
+let user = null;
+let todos = [];
+
+// Action Handlers
+async function handlePageLoad() {
+    user = getUser();
+    protectPage(user);
+
+    // *** set todos state from get all service function
+    todos = await getAllTodos();
+
+    display();
+}
+
+async function handleSignOut() {
+    signOut();
+}
+
+async function handleAdd(task) {
+    // *** 
+    // 1. create a new todo with description set to task and complete false
+    const todo = await createTodo({
+        description: task,
+        complete: false
+    });
+    // 2. push the new todo into the todos array
+    todos.push(todo);
+
+    display();
+}
+
+async function handleComplete(todo) {
+    // *** 
+    // 1. Toggle todo complete property
+    todo.complete = !todo.complete;
+    // 2. Get the index of the current todo
+    const index = todos.indexOf(todo);
+    // 3. Update that index of the array with the result of the update service function
+    todos[index] = await updateTodo(todo);
+
+    display();
+}
+
+async function handleEdit(todo, task) {
+    // ***
+    // 1. Set the todo description to the new task text
+    todo.description = task;
+    // 2. Get the index of the current todo
+    const index = todos.indexOf(todo);
+    // 3. Update that index of the array with the result of the update service function
+    todos[index] = await updateTodo(todo);
+
+    display();
+}
+
+async function handleDelete(todo) {
+    // ***
+    // 1. Get the index of the current todo
+    const index = todos.indexOf(todo);
+    // 2. Call the delete service function
+    await deleteTodo(todo);
+    // 3. remove the todo from the todos array using splice
+    todos.splice(index, 1);
+
+    display();
+}
+
+const Todos = createTodos(document.querySelector('.todo-list'), {
+    handleComplete,
+    handleEdit,
+    handleDelete
+});
+
+const AddTodo = createAddTodo(document.querySelector('.new-todo'), { handleAdd });
+
+// Components 
+const User = createUser(
+    document.querySelector('#user'),
+    { handleSignOut }
+);
+
+function display() {
+    User({ user });
+    Todos({ todos });
+    AddTodo();
+}
+
+handlePageLoad();
---
+++ services/todo-service.js
@@ -0,0 +1,42 @@
+import { client } from './client.js';
+
+export async function getAllTodos() {
+    // get all the todos
+    const response = await client
+        .from('todos')
+        .select();
+
+    return response.data;
+}
+
+export async function createTodo(todo) {
+    // insert a todo
+    const response = await client
+        .from('todos')
+        .insert(todo)
+        .single();
+
+    return response.data;
+}
+
+export async function updateTodo(todo) {
+    // update todo
+    const response = await client
+        .from('todos')
+        .update(todo)
+        .eq('id', todo.id)
+        .single();
+
+    return response.data;
+}
+
+export async function deleteTodo(todo) {
+    // delete todo
+    const response = await client
+        .from('todos')
+        .delete()
+        .eq('id', todo.id)
+        .single();
+        
+    return response.data;
+}
